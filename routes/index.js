39c96591752d5c1580277ac840e26b6df32f51e2
faiz <wizfaiz@icloud.com>
2024-06-19 16:52:02 -0700

    done

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ const express = require('express');
 
 const router = express.Router();
 const { getStatus, getStats } = require('../controllers/AppController');
-const { postNew } = require('../controllers/UsersController')
+const { postNew } = require('../controllers/UsersController');
 
 router.get('/status', getStatus);
 router.get('/stats', getStats);
-router.post('/users', postNew)
+router.post('/users', postNew);
 module.exports = router;
